1e565a9d5a354f20ad8f035d4f0f68cdea6ee658
Angel Reyna <63566335+AzNecry@users.noreply.github.com>
2021-10-13 20:22:55 -0700

    Puse Uses

--- PermisosVacacionales/PermisosVacacionales/frmDirectorio.cs
+++ PermisosVacacionales/PermisosVacacionales/frmDirectorio.cs
@@ -9,7 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//uses
 namespace PermisosVacacionales
 {
     public partial class frmDirectorio : Form
